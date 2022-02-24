28c0292cf50a7ba0c4cd039dc1075fba85cf9bd3
Yichen Dong <m15658011819@126.com>
2022-02-24 19:13:53 +0000

    project demo

---
+++ Project.py
@@ -0,0 +1,16 @@
+class Project:
+    def __init__(self, rolls, projectNames, completeDates, score, bestBefore):
+        self.rolls = rolls
+        self.projectNames = projectNames
+        self.completeDates = completeDates
+        self.score = score
+        self.bestBefore = bestBefore
+
+p = Project()
+'''
+rolls dict
+projectNames string
+completeDates int
+score int
+bestBefore int
+'''
